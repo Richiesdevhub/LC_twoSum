c23931ce2613654e7ab3e036877a39e18a8f8626
Ricardo Solano Fadul <risolfa@gmail.com>
2022-01-25 23:40:20 -0600

    El bueno pero puede mejorar velocidad

--- twoSum.js
+++ twoSum.js
@@ -1,22 +1,24 @@
 var twoSum = function(nums, target) {
     let output = [];
-    for (let i of nums) {
-        console.log("i:" + nums.indexOf(i));
-        for (let j of nums) {
-
-            if (nums.indexOf(i) !== nums.indexOf(j)) {
-                if (i + j == target) {
-                    output[0] = nums.indexOf(i);
-                    output[1] = nums.indexOf(j);
-                    console.log("j:" + nums.indexOf(j));
-                    return output;
+    for (let i = 0; i < nums.length; i++) {
+        if ((2 <= nums.length <= 104) || (Math.pow(-10, 9) <= nums[i] <= Math.pow(10, 9)) || (Math.pow(-10, 9) <= target <= Math.pow(10, 9))) {
+            console.log("i:" + i);
+            for (let j = 0; j < nums.length; j++) {
+                console.log("j:" + j);
+                if (i !== j) {
+                    console.log("i!==j;")
+                    if (nums[i] + nums[j] == target) {
+                        console.log("inside sum = target")
+                        output[0] = i;
+                        output[1] = j;
+                        return output
+                    }
                 }
+                console.log("after i!==j;")
             }
-            //           console.log("j:"+nums.indexOf(j));
-
         }
     }
 };
 
-console.log(twoSum([3, 3], 6))
-    // console.log(twoSum([2,7,11,15],9));
+console.log(twoSum([3, 3], 6));
+// console.log(twoSum([2,7,11,15],9));
