9a3d985b8b06f65144506dcd86adb635796c75e1
Ricardo Solano Fadul <risolfa@gmail.com>
2022-01-26 22:59:15 -0600

    me marco una diferencia

--- twoSum.js
+++ twoSum.js
@@ -1,22 +1,22 @@
 var twoSum = function(nums, target) {
     let output = [];
     for (let i = 0; i < nums.length; i++) {
-        if ((2 <= nums.length <= 104) || (Math.pow(-10, 9) <= nums[i] <= Math.pow(10, 9)) || (Math.pow(-10, 9) <= target <= Math.pow(10, 9))) {
-            console.log("i:" + i);
-            for (let j = 0; j < nums.length; j++) {
-                console.log("j:" + j);
-                if (i !== j) {
-                    console.log("i!==j;")
-                    if (nums[i] + nums[j] == target) {
-                        console.log("inside sum = target")
-                        output[0] = i;
-                        output[1] = j;
-                        return output
-                    }
+        // if ((2 <= nums.length <= 104) || (Math.pow(-10, 9) <= nums[i] <= Math.pow(10, 9)) || (Math.pow(-10, 9) <= target <= Math.pow(10, 9))) {
+        console.log("i:" + i);
+        for (let j = 0; j < nums.length; j++) {
+            console.log("j:" + j);
+            if (i !== j) {
+                console.log("i!==j;")
+                if (nums[i] + nums[j] == target) {
+                    console.log("inside sum = target")
+                    output[0] = i;
+                    output[1] = j;
+                    return output
                 }
-                console.log("after i!==j;")
             }
+            console.log("after i!==j;")
         }
+        // }
     }
 };
 
